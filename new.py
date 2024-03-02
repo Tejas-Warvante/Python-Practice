b7d304a205c546ac76e5cbf279e974a38716e76b
Tejas Warvante <tvw2602@gmail.com>
2024-03-02 23:43:56 +0530

    new updates

---
+++ new.py
@@ -0,0 +1,10 @@
+def addnumber(a,b):
+    sum=a+b
+    return sum
+
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+sum = addnumber(num1,num2)
+print(sum)
+
+
